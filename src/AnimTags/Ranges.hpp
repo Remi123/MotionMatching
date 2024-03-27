20239ba6ac774b49f1b10f489c8116d2aed06ae3
Remi <drolet.remi@gmail.com>
2024-03-26 23:00:31 -0400

    Will remove ranges.hpp

--- src/AnimTags/Ranges.hpp
+++ src/AnimTags/Ranges.hpp
@@ -360,77 +360,221 @@ void spans_union(R& out, const R& lhs,
     }
     out.resize(out_i);
 }
+#include <iterator>
 
-template<typename R>
-void masks_vectorize(R& r){
+class IndexRange {
+   public:
+    using value_type = IndexRange;
+    size_t FROM = 0;
+    size_t TO = 0;
+    // member typedefs provided through inheriting from std::iterator
+    class iterator
+        : public std::iterator<std::input_iterator_tag,  // iterator_category
+                               long,                     // value_type
+                               long,                     // difference_type
+                               const long*,              // pointer
+                               long                      // reference
+                               > {
+        friend class IndexRange;
+        size_t num = 0;
+        const size_t FROM = 0;
+        const size_t TO = 0;
 
-}
+       public:
+        explicit iterator(long _num, size_t from, size_t to)
+            : num(_num), FROM{from}, TO{to} {}
+        iterator& operator++() {
+            num = TO >= FROM ? num + 1 : num - 1;
+            return *this;
+        }
+        iterator operator++(int) {
+            iterator retval = *this;
+            ++(*this);
+            return retval;
+        }
+        iterator& operator=(const iterator& other) = default;
+        bool operator==(iterator other) const { return num == other.num; }
+        bool operator<=(iterator other) const { return num <= other.num; }
+        bool operator<(iterator other) const { return num < other.num; }
+        bool operator>=(iterator other) const { return num >= other.num; }
+        bool operator>(iterator other) const { return num > other.num; }
+        bool operator!=(iterator other) const { return !(*this == other); }
+        reference operator*() const { return num; }
+    };
+    IndexRange(size_t from, size_t to) : FROM{from}, TO{to} {}
+    IndexRange(size_t to) : FROM{0}, TO{to} {}
+    IndexRange(iterator from, iterator to) : FROM{from.num}, TO{to.num} {}
+    iterator begin() { return iterator(FROM, FROM, TO); }
+    iterator end() { return iterator(TO, FROM, TO); }
+    iterator begin() const { return iterator(FROM, FROM, TO); }
+    iterator end() const { return iterator(TO, FROM, TO); }
+    size_t front() { return FROM; }
+    size_t back() { return TO; }
+    size_t size(){return TO - FROM;}
 
-struct RangeSet : Resource {
-	GDCLASS(RangeSet, Resource);
-	PackedInt32Array anims{};
-	TypedArray<Range> anims_submasks{};
-	TypedArray<Range> ranges{};
+    IndexRange subspan(size_t offset, size_t size) const {
+        return IndexRange(offset, offset + size);
+    }
+    IndexRange subrange(size_t start, size_t end) const {
+        return IndexRange(start, end);
+    }
+    protected:
+    static void _bind_methods(){
 
-public:
-	friend RangeSet Union(const RangeSet lhs, const RangeSet rhs) {
-		RangeSet out{};
-
-		return out;
-	}
-
-protected:
-	static void _bind_methods() {
-	}
+    }
 };
 
-struct MaskSet : Resource {
-	GDCLASS(MaskSet, Resource);
+#include <assert.h>
 
-	PackedInt32Array anims{};
-	TypedArray<Range> anims_submasks{};
-	PackedInt32Array masks{};
+class IndexSet {
+    using list_type = Vector<IndexRange>;
+    using value_type = IndexRange;
+    IndexRange _lhs;
+    list_type _subviews;
 
-	void Rasterize(const PackedInt32Array &sorted_animations_index, const PackedInt32Array &mask_index) {
-		ERR_FAIL_COND(sorted_animations_index.size() != mask_index.size());
-		Clear();
-		// find number of anims
-		const std::span<const int32_t> sorted_anim_span(sorted_animations_index.ptr(), sorted_animations_index.size());
-		const size_t anim_size = sorted_anim_span.back();
-		anims.resize(anim_size);
-		anims_submasks.resize(anim_size);
-		// Assign submasks. Must be correctly sorted;
-		size_t offset = 0;
-		for (int i : std::views::iota(0) | std::views::take(anim_size)) {
-			anims[i] = i;
-			size_t size = std::ranges::count(sorted_anim_span, i);
-			if (size == 0) {
-				anims_submasks[i] = Range{ -1, -1 };
-				continue;
-			}
-			anims_submasks[i] = Range{ offset, offset + size };
-			offset += size;
-		}
-		//
-		masks = mask_index;
-	}
-	PackedVector2Array Vectorize() {
-		PackedVector2Array out{};
-		return out;
-	}
+   public:
+    using value_type = IndexRange;
+    IndexSet(const size_t start, const size_t end): _lhs{start,end},_subviews{}{
 
-	void Clear() {
-		anims.clear();
-		anims_submasks.clear();
-		masks.clear();
-	}
+    }
+    IndexSet(const IndexRange _v,
+             const std::initializer_list<IndexRange>& _sub = {})
+        : _lhs{_v}, _subviews{_sub} {}
+    IndexSet(const IndexSet& r) = default;
 
-	
-	friend MaskSet Intersection(const MaskSet lhs, const MaskSet rhs) { return {}; }
-	friend MaskSet Difference(const MaskSet lhs, const MaskSet rhs) { return {}; }
+    const IndexRange subspan(size_t offset, size_t count) const {
+        return _lhs.subspan(offset, count);
+    }
+    const IndexRange subrange(size_t index_begin, size_t index_end) const {
+        assert(index_begin <= index_end);
+        return _lhs.subrange(index_begin, index_end);
+    }
 
-public:
-protected:
-	static void _bind_methods() {
-	}
+    void operator|=(const IndexRange& _rhs) {
+        assert(_lhs.begin() <= _rhs.begin() && _rhs.end() <= _lhs.end());
+        spans_union(_subviews, list_type{_subviews}, list_type{_rhs});
+    }
+    void operator|=(const IndexSet& _rhs) {
+        spans_union(_subviews, list_type{_subviews},
+                    _rhs._subviews);
+    }
+    IndexSet operator||(const IndexSet& _rhs) const {
+        IndexSet result{_lhs};
+        spans_union(result._subviews, _subviews, _rhs._subviews);
+        return result;
+    }
+
+    void operator-=(const IndexRange& _rhs) {
+        assert(_lhs.begin() <= _rhs.begin() && _rhs.end() <= _lhs.end());
+        spans_difference(_subviews, list_type{_subviews}, {_rhs});
+    }
+    void operator-=(const IndexSet& _rhs) {
+        // assert(_lhs.begin() <= _rhs.begin() && _rhs.end() <= _lhs.end());
+        spans_difference(_subviews, list_type{_subviews},
+                         _rhs._subviews);
+    }
+    IndexSet operator-(const IndexSet& _rhs) const {
+        IndexSet result{_lhs};
+        spans_difference(result._subviews, _subviews, _rhs._subviews);
+        return result;
+    }
+
+    void operator&=(const IndexRange& _rhs) {
+        assert(_lhs.begin() <= _rhs.begin() && _rhs.end() <= _lhs.end());
+        spans_intersection(_subviews, list_type{_subviews},
+                           {_rhs});
+    }
+    IndexSet operator&&(const IndexRange& _rhs) const {
+        assert(_lhs.begin() <= _rhs.begin() && _rhs.end() <= _lhs.end());
+        IndexSet result = *this;
+        spans_intersection(result._subviews, list_type{_subviews},
+                           {_rhs});
+        return result;
+    }
+    // void operator&=(const std::vector<IndexRange>& _rhs) {
+    //     assert(_lhs.begin() <= _rhs.begin() && _rhs.end() <= _lhs.end());
+    //     spans_intersection(_subviews, std::vector<IndexRange>{_subviews},
+    //                        _rhs._subviews);
+    // }
+    IndexSet operator&&(const IndexSet& _rhs) const {
+        IndexSet result{_lhs};
+        spans_intersection(result._subviews, _subviews, _rhs._subviews);
+        return result;
+    }
+    void operator&=(const IndexSet& _rhs) {
+        spans_intersection(_subviews, list_type{_subviews},
+                           _rhs._subviews);
+    }
+
+    // IndexRange operator[](const std::size_t index) { return _subviews.; }
+    const IndexRange& operator[](const std::size_t index) const {
+        return _subviews[index];
+    }
+
+    auto begin() { return _subviews.begin(); }
+    auto end() { return _subviews.end(); }
+    list_type::ConstIterator cbegin(){return _subviews.ptr();}
+    list_type::ConstIterator cend(){return _subviews.ptr() + _subviews.size();}
+
+
+    // IndexSet RasterizeBit(size_t _mask) {
+
+    //     IndexSet out{_lhs};
+    //     out._subviews.clear();
+
+    //     bool out_active = false;
+    //     int out_i = 0;
+    //     int start = 0;
+
+    //     for (auto i :
+    //          std::views::iota(0) | std::ranges::views::take(_lhs.size())) {
+    //         std::bitset<sizeof(T)> bit = _lhs[i];
+    //         // Activate output
+    //         if (!out_active && bit.test(_mask)) {
+    //             start = i;
+    //             out_active = true;
+    //         }
+    //         // Deactivate output
+    //         else if (out_active && !bit.test(_mask)) {
+    //             out._subviews.push_back(_lhs.subspan(start, i - start));
+    //             out_active = false;
+    //             out_i++;
+    //         }
+    //     }
+    //     if (out_active) {
+    //         out._subviews.push_back(_lhs.subspan(start, _lhs.size() -
+    //         start)); out_i++;
+    //     }
+    //     return out;
+    // }
+
+    // span_view<T> RasterizeValue(const T& _mask) {
+    //     span_view<T> out{_lhs};
+    //     out._subviews.clear();
+
+    //     bool out_active = false;
+    //     int out_i = 0;
+    //     int start = 0;
+
+    //     for (auto i :
+    //          std::views::iota(0) | std::ranges::views::take(_lhs.size())) {
+    //         auto& bit = _lhs[i];
+    //         // Activate output
+    //         if (!out_active && bit == _mask) {
+    //             start = i;
+    //             out_active = true;
+    //         }
+    //         // Deactivate output
+    //         else if (out_active && bit != _mask) {
+    //             out._subviews.push_back(_lhs.subspan(start, i - start));
+    //             out_active = false;
+    //             out_i++;
+    //         }
+    //     }
+    //     if (out_active) {
+    //         out._subviews.push_back(_lhs.subspan(start, _lhs.size() -
+    //         start)); out_i++;
+    //     }
+    //     return out;
+    // }
 };
